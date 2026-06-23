b1f5d1937bf5493eed8b57be8da15869e30097af
prabhav rastogi <prabhavrastogi@prabhavs-MacBook-Air.local>
2026-06-23 22:08:35 +0530

    Fix Render deployment

--- Procfile
+++ Procfile
@@ -1,1 +1,1 @@
-web: python app.py
+web: python weather-app/app.py
--- weather-app/app.py
+++ weather-app/app.py
@@ -105,6 +105,8 @@ def forecast():
         
     
 if __name__ == "__main__":
-    app.run(debug=True, port=5001)
-
+    app.run(
+        host="0.0.0.0",
+        port=int(os.environ.get("PORT",10000))
+    )
 
